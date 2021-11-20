8a1df4ee4369e7a4b057b9de5ce05d830f92385e
Venkat <58289784+Venki22@users.noreply.github.com>
2021-11-20 20:22:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/mithuntechnologies', function(request, response) {
     //response.send("<h2><center>Welcome to Node JS app</h2>");
     response.write("<h2><center><u>Node JS  Application </u></center></h2>");
 	
-    response.write("<h2><center>Welcome to  Mithun Technologies. Please Contact +91-9980923226 for more information or send an email to devopstrainingblr@gmail.com <center></h2>" );
+    response.write("<h4><center>Welcome to  Mithun Technologies. Please Contact +91-9176998809 for more information or send an email to devopstrainingblr@gmail.com <center></h4>" );
     response.end();
     
   })
