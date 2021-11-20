51b4f6833880217657cf8194ab10d6e4d257a9fb
Venkat <58289784+Venki22@users.noreply.github.com>
2021-11-20 21:20:47 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,17 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.110.48.33:8222/',
+   //Linxu EC2
+  //serverUrl: 'http://3.110.48.33:8222/',
+  //Windows
+    serverUrl: 'http://localhost:9111/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    //'sonar.password': 'admin',
+	     'sonar.password': 'Oct2021$',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
